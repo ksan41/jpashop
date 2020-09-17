ea3aa1c8aa7e988cb0e93a1ef18c5a622de6df62
ksan41 <aquam41@gmail.com>
2020-09-18 00:59:55 +0900

    Update ItemService.java

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+
+    @Transactional
+    public void updateItem(Long itemId, UpdateItemDto itemDto){
+        // 영속성 엔티티를 find메소드로 찾아옴(jpa가 변경을 감지하고 있음)
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(itemDto.getName());
+        findItem.setPrice(itemDto.getPrice());
+        findItem.setStockQuantity(itemDto.getStockQuantity());
     }
 
     public List<Item> findItems(){
