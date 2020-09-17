d4769d4aa5d29a186b54b2c7ae0a789e81bd04ef
ksan41 <aquam41@gmail.com>
2020-09-18 00:59:50 +0900

    Update ItemController.java

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.controller;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.UpdateItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -66,17 +67,23 @@ public class ItemController {
 
 
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@PathVariable String itemId,@ModelAttribute("form") BookForm form){
+    public String updateItem(@PathVariable Long itemId,@ModelAttribute("form") BookForm form){
 
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
 
-        itemService.saveItem(book);
+        UpdateItemDto itemDto = new UpdateItemDto();
+        itemDto.setName(form.getName());
+        itemDto.setPrice(form.getPrice());
+        itemDto.setStockQuantity(form.getStockQuantity());
+
+        itemService.updateItem(itemId,itemDto);
+
         return "redirect:/items";
     }
 
