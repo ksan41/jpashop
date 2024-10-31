8652f4134be2ca62d2e507390c2c6b49bc4fcfe5
ksan <aquam41@gmail.com>
2024-10-31 16:21:58 +0900

    [Feat] Order조회 api 추가(dto 한방쿼리 조회)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -77,6 +79,21 @@ public class OrderApiController {
     @GetMapping("api/v5/orders")
     public List<OrderQueryDto> getOrdersv5() {
         return orderQueryRepository.findAllByDtos();
+    }
+
+    @GetMapping("api/v6/orders")
+    public List<OrderQueryDto> getOrdersv6() {
+        List<OrderFlatDto> orderFlatDtos = orderQueryRepository.findAllByDtosFlat();
+        return orderFlatDtos.stream()
+                .collect(Collectors.groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        Collectors.mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), Collectors.toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(Collectors.toList());
     }
 
     @Data
